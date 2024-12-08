56947d5517b0885b856a3ae9798ccfe6c21cf75c
Pranavi2502 <pandiripranavi25@gmail.com>
2024-12-08 18:53:08 +0530

    first commit

---
+++ Prime.java
@@ -0,0 +1,19 @@
+public class Prime {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        for (int j = 2; j <= Math.sqrt(num); j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
